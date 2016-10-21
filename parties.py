6d7d03f57d9961c23bf29afe43ca253fb3b30b14
neal-o-r <neal@n-o-r.xyz>
2016-10-21 19:31:23 +0100

    started splitting the code up

---
+++ parties.py
@@ -0,0 +1,5 @@
+
+affiliation = {"FG": "Fine Gael", "LAB": "Labour Party", "FF": "Fianna Fáil", "SF":"Sinn Féin", "AAA":"Ant-Austerity Alliance", "CC":"Ceann Comhairle", "GRN":"Green Party", "IND":"Independents", "PBP":"People Before Profits", "SD":"Social Democrats", "ULP":"United Labour Party", "WUA":"Workers and Unemployed Action"}
+
+colour = {"FG": "#6699FF", "LAB": "#CC0000", "FF": "#66BB66", "SF":"#008800", "AAA":"#E5E500", "CC":"yellow", "GRN":"#99CC33", "IND":"Black", "PBP":"#E5E500", "SD":"#752F8B", "ULP":"#CC0000", "WUA":"#D73D3D"}
+
