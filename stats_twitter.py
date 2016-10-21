65cffc36c6a69a433e407a9c306b8da77cc3c883
neal-o-r <neal@n-o-r.xyz>
2016-10-21 23:33:27 +0100

    made figures

---
+++ stats_twitter.py
@@ -0,0 +1,83 @@
+import tweepy as tw
+import pandas as pd
+import seaborn as sns; sns.set()
+import parties
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+def stats(handle):
+	
+	print(handle)
+	try:	
+		user = api.get_user(handle)
+
+		followers = user.followers_count
+		tweets    = user.statuses_count
+		out = pd.Series({'Followers':followers, 'Tweets':tweets})
+	except:
+		out = pd.Series({'Followers':0, 'Tweets':0})
+
+	return out
+
+
+with open("api.keys", 'r') as f:
+
+        keys = f.readlines()
+        
+
+for i, key in enumerate(keys):
+        keys[i] = key.rstrip('\n')
+        
+
+auth = tw.OAuthHandler(keys[0], keys[1])
+auth.set_access_token(keys[2], keys[3])
+
+api = tw.API(auth)
+
+df = pd.read_csv('twits.csv')
+
+df['Colour'] = df['Party'].map(parties.colour)
+
+none_series = pd.Series({'Followers': 0, 'Tweets':0})
+
+df[['Followers', 'Tweets']] = df.Handle.apply(lambda x: stats(x) if x != None else none_series)
+	
+df = df.set_index(df.Name)
+
+df = df[df.Followers != 0]
+df = df.sort(['Followers'])
+
+legend_patches = []
+
+for i in parties.colour:
+
+	patch = mpatches.Patch(color=parties.colour[i], label=i)
+	legend_patches.append(patch)
+
+
+labels = [i.get_label() for i in legend_patches]
+
+df.Followers.plot(kind='bar', color=df.Colour, fontsize=7)
+plt.ylabel('Followers')
+plt.legend(legend_patches, labels)
+
+plt.show()
+
+
+df = df.sort(['Tweets'])
+df.Tweets.plot(kind='bar', color=df.Colour, fontsize=7)
+plt.ylabel('Tweets')
+plt.legend(legend_patches, labels)
+
+
+plt.show()
+
+
+df = df[(df.Party != 'ULP') & (df.Party != 'CC') & (df.Party != 'WUA')]
+df.boxplot(column='Followers', by='Party')
+
+plt.ylim([0,30000])
+plt.ylabel('Followers')
+plt.show()
+
+
--- twit_list.py
+++ twit_list.py
@@ -1,23 +1,56 @@
-import urllib
-#import HTMLParser
-#pars = HTMLParser.HTMLParser()
+import urllib.request as urlreq
+import html
 
 
 url = "https://www.kildarestreet.com/tds/"
-response = urllib.request.urlopen(url)
-html = response.readlines()
+response = urlreq.urlopen(url)
+webpage = response.readlines()
 
-names, parties = [], []
-for index, value in enumerate(html):
+names, parties, twits = [], [], []
+for index, value in enumerate(webpage):
 
-        value = value.decode()
-        try:
-                if "/td/" in value:
+	value = html.unescape(value.decode('utf-8'))
+	try:
+		if "/td/" in value:
 
-                        name = value.split('"')[-1][1:].split("<")[0]
-                        names.append(name)                
-                       # address = value.split('"')[3]
-                       # party = html[index + 1].split(">")[1].split("<")[0]    
-                        print(name, party)
-        except:
-                continue
+			name = value.split('"')[-1][1:].split("<")[0]
+
+			names.append(name)                
+
+			address = value.split('"')[3]
+			next_line = html.unescape(webpage[index + 1].decode('utf-8'))
+			party = next_line.split(">")[1].split("<")[0]    
+			
+			parties.append(party)	
+
+			url2 = "https://www.kildarestreet.com" + address
+			response2 = urlreq.urlopen(url2)
+			webpage2 = response2.readlines()
+
+			twit_switch = False
+			for line_feed in webpage2:
+				line_feed = html.unescape(line_feed.decode('utf-8'))				
+
+				if "http://twitter" in line_feed:
+
+					twitter_add = line_feed.split('http://twitter.com/')[1].split('"')[0]
+					twit_switch = True
+					break
+				
+			if twit_switch == False:
+				twitter_add = None
+
+			twits.append(twitter_add)
+
+	
+	except:
+		continue
+
+f = open('twits.csv', 'w')
+
+for i in range(len(names)):
+    f.write(names[i] +','+parties[i]+','+str(twits[i])+'\n')
+
+f.close()
+
+
