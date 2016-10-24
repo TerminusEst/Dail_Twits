1515cb950bfa23e997b8cd90a9a9631ca1fe43b8
neal-o-r <neal@n-o-r.xyz>
2016-10-24 13:29:32 +0100

    tidied up a bit

--- stats_twitter.py
+++ stats_twitter.py
@@ -1,9 +1,6 @@
 import tweepy as tw
 import pandas as pd
-import seaborn as sns; sns.set()
 import parties
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 def stats(handle):
 	
@@ -34,6 +31,7 @@ auth.set_access_token(keys[2], keys[3])
 
 api = tw.API(auth)
 
+
 df = pd.read_csv('twits.csv')
 
 df['Colour'] = df['Party'].map(parties.colour)
@@ -44,40 +42,4 @@ df[['Followers', 'Tweets']] = df.Handle.apply(lambda x: stats(x) if x != None el
 	
 df = df.set_index(df.Name)
 
-df = df[df.Followers != 0]
-df = df.sort(['Followers'])
-
-legend_patches = []
-
-for i in parties.colour:
-
-	patch = mpatches.Patch(color=parties.colour[i], label=i)
-	legend_patches.append(patch)
-
-
-labels = [i.get_label() for i in legend_patches]
-
-df.Followers.plot(kind='bar', color=df.Colour, fontsize=7)
-plt.ylabel('Followers')
-plt.legend(legend_patches, labels, loc=2)
-
-plt.show()
-
-
-df = df.sort(['Tweets'])
-df.Tweets.plot(kind='bar', color=df.Colour, fontsize=7)
-plt.ylabel('Tweets')
-plt.legend(legend_patches, labels)
-
-
-plt.show()
-
-
-df = df[(df.Party != 'ULP') & (df.Party != 'CC') & (df.Party != 'WUA')]
-df.boxplot(column='Followers', by='Party')
-
-plt.ylim([0,30000])
-plt.ylabel('Followers')
-plt.show()
-
-
+df.to_csv('twit_stats.csv')
