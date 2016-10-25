2f29a625cd2e7deaae90d60a97d1050e642e39f5
neal-o-r <neal@n-o-r.xyz>
2016-10-25 11:05:18 +0100

    added cumulative plot

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Plotting the twitter followers of Irish TD's
 
 ![alt tag](https://github.com/neal-o-r/Dail_Twits/blob/master/figs/most_followed.png)
+![alt tag](https://github.com/neal-o-r/Dail_Twits/blob/master/figs/cumulative.png)
 ![alt tag](https://github.com/neal-o-r/Dail_Twits/blob/master/figs/followers.png)
 ![alt tag](https://github.com/neal-o-r/Dail_Twits/blob/master/figs/tweets.png)
 ![alt tag](https://github.com/neal-o-r/Dail_Twits/blob/master/figs/boxplot.png)
--- make_charts.py
+++ make_charts.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import seaborn as sns.set()
+import seaborn as sns; sns.set()
 import parties
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
+import datetime as dt
 
 def plot_bar(df, column):
         
@@ -25,12 +25,26 @@ def plot_bar(df, column):
         plt.show()
 
 
-df = pd.read_csv('twit_stats.csv')
+df = pd.read_csv('twit_stats.csv', parse_dates=[5])
 
 df = df[df.Followers != 0]
+df = df[df.Handle != 'None']
+
+
 
 plot_bar(df, 'Followers')
 plot_bar(df, 'Tweets')
+
+df['Created'] = pd.to_datetime([i for i in df.Created], yearfirst=True)
+df = df.sort(['Created'])
+
+plt.plot(df.Created, range(len(df)))
+
+plt.ylim([0,166])
+plt.xlim([dt.date(2006, 3, 21), dt.datetime.now()])
+plt.ylabel('Number of TDs on Twitter')
+plt.xlabel('Date')
+
 
 df = df[(df.Party != 'ULP') & (df.Party != 'CC') & (df.Party != 'WUA')]
 df.boxplot(column='Followers', by='Party')
@@ -38,3 +52,5 @@ df.boxplot(column='Followers', by='Party')
 plt.ylim([0,30000])
 plt.ylabel('Followers')
 plt.show()
+
+
